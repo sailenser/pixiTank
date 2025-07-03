a3f01bcaf4773e7e9c475ae34a42399306c7c6a6
baranov.m <baranov.m@promail366.com>
2025-07-03 18:16:44 +0300

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   server: {
     port: 8080,
     open: true,
+    headers: {
+      "Content-Type": "application/javascript",
+    },
   },
   resolve: {
     alias: {
