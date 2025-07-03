80468182443660634c429c2f56c2140181263b7d
baranov.m <baranov.m@promail366.com>
2025-07-03 23:57:08 +0300

    config fix 5

--- src/AssetLoader.ts
+++ src/AssetLoader.ts
@@ -4,35 +4,35 @@ export async function preloadImages() {
   const assets = [
     {
       alias: "GunConnectorD",
-      src: "./public/assets/parts/gun_connectors/GunConnectorD.png",
+      src: "./assets/parts/gun_connectors/GunConnectorD.png",
     },
     {
       alias: "HeavyGunB",
-      src: "/src/assets/parts/guns/HeavyGunB.png",
+      src: "./assets/parts/guns/HeavyGunB.png",
     },
 
     {
       alias: "HeavyHullB",
-      src: "/src/assets/parts/hulls/HeavyHullB.png",
+      src: "./assets/parts/hulls/HeavyHullB.png",
     },
 
     {
       alias: "HeavyTowerB",
-      src: "/src/assets/parts/towers/HeavyTowerB.png",
+      src: "./assets/parts/towers/HeavyTowerB.png",
     },
 
     {
       alias: "TrackСFrame1",
-      src: "/src/assets/parts/tracks/TrackСFrame1.png",
+      src: "./assets/parts/tracks/TrackСFrame1.png",
     },
     {
       alias: "TrackСFrame2",
-      src: "/src/assets/parts/tracks/TrackСFrame2.png",
+      src: "./assets/parts/tracks/TrackСFrame2.png",
     },
 
     {
       alias: "MediumShell",
-      src: "/src/assets/parts/bullets/MediumShell.png",
+      src: "./assets/parts/bullets/MediumShell.png",
     },
   ];
 
